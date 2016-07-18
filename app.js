01b9fbd3db2b871f4578b6ccae69b51c6ec513e6
Arshad Shaik <evercoolarshad@gmail.com>
2016-07-19 02:57:01 +0530

    Templating Engines Module

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+app.set('view engine','ejs');
 
 app.get('/',function(req,res) {
-    res.send('Hello World');
+    res.render('index',{title: 'Hello from render',nav:['Books','Authors']});
 });
 app.get('/books',function(req,res) {
     res.send('Hello Books');
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
-
+var nodemon = require('gulp-nodemon');
 var jsFiles = ['*.js','src/**/*.js'];
 
 gulp.task('style', function() {
@@ -25,8 +25,23 @@ gulp.task('inject',function() {
         directory: './public/lib',
         ignorePath: '../../public'
     };
-    return gulp.src('./src/views/*.html')
+    return gulp.src('./src/views/*.jade')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc,injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve',['style','inject'],function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT':3000,
+        },
+        watch: jsFiles
+    }
+    return nodemon(options)
+        .on('restart',function(ev){
+            console.log('Restarting....');
+        });
 });
