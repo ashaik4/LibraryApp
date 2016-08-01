0c68f9f0d683fc0ac93658e437f9d1007bad173b
Arshad Shaik <evercoolarshad@gmail.com>
2016-08-01 15:58:39 -0500

    Implemented passport-local strategy.

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,9 +2,28 @@ var passport = require('passport');
 
 
 
-module.exports = function(app) {
+module.exports = function(app){
 
     app.use(passport.initialize());
     app.use(passport.session());
+
+    //serialize: to bundle up user with the session(usually, we store the ID)
+
+    //deserialize: to pull the user back out of the session.
+
+    //Passport User Functions
+
+    // * passport.serializeUser()
+    // * passport.deserializeUser()
+    // * passport-local
+
+    passport.serializeUser(function(user,done) {
+        done(null, user);
+
+        passport.deserializeUser(function (user, done) {
+            done(null, user);
+        });
+        require('./strategies/local.strategy')();
+    });
 
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,14 @@ var mongodb = require('mongodb').MongoClient;
 var router = function(nav){
     authRouter.route('/signUp').post(function(req,res){
         console.log(req.body);
+        req.login(req.body,function(){
+            res.redirect('/auth/profile');
+        });
     });
+    authRouter.route('/profile')
+        .get(function(req,res){
+            res.json(req.user);
+        });
     return authRouter;
 };
 
